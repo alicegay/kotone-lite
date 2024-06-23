d07a17e3c7795f3de80cc0edab3eb7062195dc7e
Alice <alice@sleepyand.gay>
2024-06-24 02:12:58 +1000

    initial

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { registerRootComponent } from 'expo'
+import { View, Text } from 'react-native'
+
+const App = () => {
+  return (
+    <View>
+      <Text>KOTONE!</Text>
+    </View>
+  )
+}
+
+export default registerRootComponent(App)
